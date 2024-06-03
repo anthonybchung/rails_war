d7c01dd2e0b6382ebb393decd097083e6f2ce162
anthony.b.chung <anthony.b.chung@gmail.com>
2024-06-03 15:49:24 +1000

    sign in view styling

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Devise will be the one tracking the user.
 ```console
 gem 'devise', '~> 4.9', '>= 4.9.3'
 ```
+
+```console
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+```
+Modify the above ***host: 'actual_host'*** for the hosting website.
+
+## Structure the Devise Views.
+
+### new_user_session (sign_in):GET
+
+
+
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    colors: {
+      'primary': '#7674EF',
+      'secondary': '#B2B1F0',
+      'tertiary': '#EEEEFF',
+      'text': '#01002C',
+      'white': '#ffffff'
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
